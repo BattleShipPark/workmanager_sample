1338e5e9fc2f92787591a4cad34de9ca1e234452
BattleShip Park <battleship.park@gmail.com>
2020-06-10 21:12:12 +0900

    add constraints

--- app/src/main/java/com/battleshippark/workmanagersample/MainActivity.kt
+++ app/src/main/java/com/battleshippark/workmanagersample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.battleshippark.workmanagersample
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.Constraints
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
@@ -16,7 +17,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClick(view: View) {
-        val workRequest = PeriodicWorkRequestBuilder<HeavyWorker>(1, TimeUnit.DAYS).build()
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiresDeviceIdle(true)
+            .build()
+
+        val workRequest = PeriodicWorkRequestBuilder<HeavyWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
         WorkManager.getInstance(this)
             .enqueueUniquePeriodicWork(
                 "unique_heavy_work",
